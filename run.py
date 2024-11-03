dfe25c064f3255f19d33427b5bd4b1183ce0265d
Jassic <answotlr54@snu.ac.kr>
2024-11-04 04:16:41 +0900

    port

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)  # 필요에 따라 debug 옵션 사용
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
